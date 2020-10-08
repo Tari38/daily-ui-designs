9aaff5c70b4172a342c0ac0f9e9ac99f2fcf3dc5
Samantha <51409073+Tari38@users.noreply.github.com>
2020-10-08 11:49:35 +0100

    add build files

---
+++ build/precache-manifest.94c6ee9efcfcf32699bedd1c3b53d1a9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732baf606f03072d86f84afc0ed6f3ef",
+    "url": "/Tari38/daily-ui-designsindex.html"
+  },
+  {
+    "revision": "af19bd4c7aad1e0c19ad",
+    "url": "/Tari38/daily-ui-designsstatic/css/main.520abf3c.chunk.css"
+  },
+  {
+    "revision": "68f3294486eead332bda",
+    "url": "/Tari38/daily-ui-designsstatic/js/2.f4e61a12.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Tari38/daily-ui-designsstatic/js/2.f4e61a12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af19bd4c7aad1e0c19ad",
+    "url": "/Tari38/daily-ui-designsstatic/js/main.d482495d.chunk.js"
+  },
+  {
+    "revision": "22089180b990bccd44c5",
+    "url": "/Tari38/daily-ui-designsstatic/js/runtime-main.f7ff5454.js"
+  },
+  {
+    "revision": "00f28743c1059e2afa10acf2dec66c66",
+    "url": "/Tari38/daily-ui-designsstatic/media/1.00f28743.png"
+  },
+  {
+    "revision": "b2a86bd045e9d3162e02664574257f9c",
+    "url": "/Tari38/daily-ui-designsstatic/media/1.b2a86bd0.png"
+  },
+  {
+    "revision": "3dd62df9eb6f44c778e18d03174ef73f",
+    "url": "/Tari38/daily-ui-designsstatic/media/2.3dd62df9.png"
+  },
+  {
+    "revision": "cea0461216dcf8e4ab9f3d60d37143ad",
+    "url": "/Tari38/daily-ui-designsstatic/media/2.cea04612.png"
+  },
+  {
+    "revision": "3432c97d2fe45fe698240b0b7703a0e9",
+    "url": "/Tari38/daily-ui-designsstatic/media/3.3432c97d.png"
+  },
+  {
+    "revision": "943a2c745ebddd3b8985e48ddb19606d",
+    "url": "/Tari38/daily-ui-designsstatic/media/3.943a2c74.png"
+  },
+  {
+    "revision": "59152822727a9bca8ba4b133fa5dc1fd",
+    "url": "/Tari38/daily-ui-designsstatic/media/4.59152822.png"
+  },
+  {
+    "revision": "8140f7f881ca08bc90bcb694734e9507",
+    "url": "/Tari38/daily-ui-designsstatic/media/signupImg1.8140f7f8.png"
+  },
+  {
+    "revision": "7dfe40763c1fb56272571a1a14eca95a",
+    "url": "/Tari38/daily-ui-designsstatic/media/signupImg2.7dfe4076.png"
+  }
+]);
