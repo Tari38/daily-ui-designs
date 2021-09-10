11ddc10db76ba32eadf3c3c6db10c30125ee55f3
Samantha <51409073+Tari38@users.noreply.github.com>
2021-09-10 10:30:40 +0100

    added images
    to static web design

--- src/components/design-modules/static/StaticPages.jsx
+++ src/components/design-modules/static/StaticPages.jsx
@@ -4,6 +4,9 @@ import StaticImg1 from "./images/Header.png";
 import StaticImg2 from "./images/Intro.png";
 import StaticImg3 from "./images/Portfolio.png";
 import StaticImg4 from "./images/Contact.png";
+import StaticImg5 from "./images/1.png";
+import StaticImg6 from "./images/2.png";
+import StaticImg7 from "./images/3.png";
 
 import "./StaticPages.css";
 
@@ -35,6 +38,21 @@ export default class StaticPages extends Component {
           <Carousel.Item>
           <div className="d-block">
             <img src={StaticImg4} className="d-block w-100" alt="static 4"></img>
+          </div>
+          </Carousel.Item>
+					<Carousel.Item>
+          <div className="d-block">
+            <img src={StaticImg5} className="d-block w-100" alt="static 5"></img>
+          </div>
+          </Carousel.Item>
+					<Carousel.Item>
+          <div className="d-block">
+            <img src={StaticImg6} className="d-block w-100" alt="static 6"></img>
+          </div>
+          </Carousel.Item>
+					<Carousel.Item>
+          <div className="d-block">
+            <img src={StaticImg7} className="d-block w-100" alt="static 7"></img>
           </div>
           </Carousel.Item>
           </Carousel>
